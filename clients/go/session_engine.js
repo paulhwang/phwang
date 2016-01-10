ac24efbcc4079dc9837d965217635578108d1cca
paul.hwang (黃文樸 - Getac) <paul.hwang@getac.com.tw>
2016-01-10 20:30:54 +0800

    clean up session_mgr

--- clients/go/session_engine.js
+++ clients/go/session_engine.js
@@ -4,11 +4,10 @@
  * File name: SessionEngineObject.js
  */
 
-function SessionEngineObject(root_object_val, port_object_val, session_object_val) {
+function SessionEngineObject(root_object_val, session_object_val) {
     "use strict";
     this.theObjectName = "SessionEngineObject";
     this.theRootObject = root_object_val;
-    this.thePortObject = port_object_val;
     this.theSessionObject = session_object_val;
 
     this.objectName = function () {
@@ -17,10 +16,6 @@ function SessionEngineObject(root_object_val, port_object_val, session_object_va
 
     this.rootObject = function () {
         return this.theRootObject;
-    };
-
-    this.portObject = function () {
-        return this.thePortObject;
     };
 
     this.sessionObject = function () {
@@ -37,26 +32,6 @@ function SessionEngineObject(root_object_val, port_object_val, session_object_va
 
     this.ajxObject = function () {
         return this.rootObject().ajxObject();
-    };
-
-    this.configObject = function () {
-        return this.containerObject().configObject();
-    };
-
-    this.containerObject2 = function () {
-        return this.containerObject().containerObject2();
-    };
-
-    this.httpGetRequest = function () {
-        return this.theHttpGetRequest;
-    };
-
-    this.httpPostRequest = function () {
-        return this.theHttpPostRequest;
-    };
-
-    this.sendHttpGetRequest______ = function (session_val) {
-         this.ajxObject().getMessage(this.httpGetRequest(),this.ajxObject().jsonContext(), this, session_val);
     };
 
     this.setupSession = function (session_val) {
@@ -82,34 +57,8 @@ function SessionEngineObject(root_object_val, port_object_val, session_object_va
         }
     };
 
-    this.transmitStringData = function (str_val) {
-        //GO.goLog("SessionMgrObject.transmitStringData", str_val);
-        if (this.configObject().playBothSides()) {
-            //this.receiveStringData(str_val);
-            if (this.ajxObject()) {
-                this.ajxObject().postMessage(str_val, session.hisName());
-                this.ajxObject().getMessage(this, session.myName());
-             }
-            return;
-        }
-
-        if (this.containerObject2()) {
-            if (this.containerObject2().portObject()) {
-                this.containerObject2().portObject().sessionEngineObject().receiveStringData(str_val);
-                return;
-            }
-        }
-    };
-
     this.receiveStringData = function (str_val) {
-        //GO.goLog("SessionMgrObject.receiveStringData", str_val);
-
-        //this.sessionObject().receiveQueue().enQueue(str_val);
-        //str_val = this.sessionObject().receiveQueue().deQueue();
         this.sessionObject().receiveData(str_val);
-        //this.sessionObject().theReceiveFunction(this.sessionObject().targetReceiveObject(), str_val);
-
-        //this.portObject().receiveStringData(str_val);
     };
 
     this.abend = function (str1_val, str2_val) {
@@ -119,8 +68,5 @@ function SessionEngineObject(root_object_val, port_object_val, session_object_va
     this.logit = function (str1_val, str2_val) {
         return this.utilObject().utilLogit(this.objectName() + "." + str1_val, str2_val);
     };
-
-    this.theHttpGetRequest = this.ajxObject().newHttpRequest();
-    this.theHttpPostRequest = this.ajxObject().newHttpRequest();
 }
 
