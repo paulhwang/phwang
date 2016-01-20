03fe9b5da78fdccafa9b06d6eb4b092b4a570fd5
paul.hwang (黃文樸 - Getac) <paul.hwang@getac.com.tw>
2016-01-20 23:50:29 +0800

    ui

--- clients/go/go_game.js
+++ clients/go/go_game.js
@@ -139,6 +139,10 @@ function GoGameObject(container_val, str_val) {
         if (!this.engineObject().isValidMoveOnBoard(x_val, y_val)) {
             return;
         }
+
+        if (this.uiObject().outstandingUiClick() !== 0) {
+            return;
+        }
         this.uiObject().incrementOutstandingUiClick();
 
         var move = new GoMoveObject(null, x_val, y_val, this.nextColor(), this.totalMoves(), this.containerObject());
