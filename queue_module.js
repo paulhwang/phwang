fca9ab3216806008f1173a3ae60c219d3b25c204
Paul Hwang <paul.hwang@getac.com.tw>
2016-01-21 14:33:47 +0800

    queue

--- queue_module.js
+++ queue_module.js
@@ -4,24 +4,23 @@
  * File name: queue_module.js
  */
 
-var util = require("./util_module.js");
 var holder_pool = require("./holder_pool_module.js");
 
 module.exports = {
     malloc: function () {
         que = new QueueObject();
-        que.head = null;
-        que.tail = null;
-        que.size = 0;
         return que;
     },
 
     enqueue: function (queue_val, data_val) {
-        enQueue(queue_val, data_val);
+        if (!queue_val) {
+            return;
+        }
+        queue_val.enQueue(data_val);
     },
 
     dequeue: function (queue_val) {
-        return deQueue(queue_val);
+        return queue_val.deQueue();
     },
 
     queue_size: function (queue_val) {
@@ -29,200 +28,170 @@ module.exports = {
     },
 
     remove: function (queue_val, func_val, input_val1, input_val2, input_val3) {
-        removeElement(queue_val, func_val, input_val1, input_val2, input_val3);
+        queue_val.removeElement(func_val, input_val1, input_val2, input_val3);
     },
 
     search: function (queue_val, func_val, input_val1, input_val2, input_val3) {
-        return searchIt(queue_val, func_val, input_val1, input_val2, input_val3);
+        return queue_val.searchIt(func_val, input_val1, input_val2, input_val3);
     },
-
 };
 
 function QueueObject () {
-    //var head;
-    //var tail;
-    //var size;
-}
-
-function enQueue (queue_val, data_val) {
     "use strict";
 
-    if (!queue_val) {
-        abend('enqueueIt', 'null queue_val');
-        return;
-    }
+    this.objectName = function () {
+        return "QueueObject";
+    };
 
-    if (!data_val) {
-        abend('enqueueIt', 'null data_val');
-        return;
-    }
+    this.utilModule = function () {
+        return this.theUtilModule;
+    };
 
-    //logit("enqueueIt", "start");
-
-    abendIt(queue_val);
-
-    var data_entry = holder_pool.malloc(data_val);
-    if (!data_entry) {
-        abend('enqueueIt', 'null data_entry');
-        return;
-    }
-
-    queue_val.size += 1;
-    if (!queue_val.head) {
-        //logit("enqueueIt", "1000");
-        data_entry.prev = null;
-        data_entry.next = null;
-        queue_val.head = data_entry;
-        queue_val.tail = data_entry;
-    } else {
-        //logit("enqueueIt", "2000");
-        queue_val.tail.next = data_entry;
-        data_entry.prev = queue_val.tail;
-        data_entry.next = null;
-        queue_val.tail = data_entry;
-    }
-    abendIt(queue_val);
-}
-
-function deQueue (queue_val) {
-    "use strict";
-    var data_entry;
-    var data;
-
-    //logit("deQueue", "start");
-
-    if (!queue_val) {
-        return;
-    }
-
-    //logit("dequeueIt", "1000");
-
-    abendIt(queue_val);
-
-    if (!queue_val.head) {
-        //logit("dequeueIt", "2000");
-        data_entry = null;
-        data = null;
-    } else if (queue_val.head === queue_val.tail) {
-        //logit("dequeueIt", "3000");
-        queue_val.size -= 1;
-        data_entry = queue_val.head;
-        data = data_entry.data;
-        queue_val.head = null;
-        queue_val.tail = null;
-    } else {
-        //logit("dequeueIt", "4000");
-        queue_val.size -= 1;
-        data_entry = queue_val.head;
-        data = data_entry.data;
-        queue_val.head = queue_val.head.next;
-        queue_val.head.prev = null;
-    }
-
-    //logit("dequeueIt", "5000");
-
-    if (data_entry) {
-        //logit("dequeueIt", "data=" + data_entry.data);
-        holder_pool.free(data_entry);
-    }
-    else {
-        //logit("dequeueIt", "6000");
-    }
-
-    abendIt(queue_val);
-
-    //logit("dequeueIt", "end");
-    return data;
-}
-
-function removeElement (queue_val, func_val, input_val1, input_val2, input_val3) {
-    var p;
-
-    debug(false, "removeElement", "start");
-    debug(false, "removeElement", func_val);
-    abendIt(queue_val);
-
-    p = queue_val.head;
-    while (p) {
-        debug(false, "removeElement", "in while loop");
-        if (func_val(p.data, input_val1, input_val2, input_val3)) {
-            debug(false, "removeElement", "found");
-            if (p.prev) {
-                p.prev.next = p.next;
-            } else {
-                queue_val.head = p.next;
-            }
-            if (p.next) {
-                p.next.prev = p.prev;
-            } else {
-                queue_val.tail = p.prev;
-            }
-            queue_val.size -= 1;
+    this.enQueue = function (data_val) {
+        if (!data_val) {
+            this.abend("enQueue", "null data_val");
             return;
         }
-        p = p.next;
-    }
-    abendIt(queue_val);
-    debug(false, "removeElement", "not found");
-}
 
-function searchIt(queue_val, func_val, input_val1, input_val2, input_val3) {
-    var p;
+        this.abendIt();
 
-    debug(false, "searchIt", "start");
-    debug(false, "searchIt", func_val);
-    p = queue_val.head;
-    while (p) {
-        debug(false, "searchIt", "in while loop");
-        if (func_val(p.data, input_val1, input_val2, input_val3)) {
-            debug(false, "searchIt", "found");
-            return p.data;
+        var data_entry = holder_pool.malloc(data_val);
+        if (!data_entry) {
+            this.abend("enQueue", "null data_entry");
+            return;
         }
-        p = p.next;
-    }
-    debug(false, "searchIt", "not found");
-    return null;
-}
 
-function abendIt (queue_val) {
-    "use strict";
-    var i, p;
+        this.size += 1;
+        if (!this.head) {
+            //logit("enqueueIt", "1000");
+            data_entry.prev = null;
+            data_entry.next = null;
+            this.head = data_entry;
+            this.tail = data_entry;
+        } else {
+            //logit("enqueueIt", "2000");
+            this.tail.next = data_entry;
+            data_entry.prev = this.tail;
+            data_entry.next = null;
+            this.tail = data_entry;
+        }
+        this.abendIt();
+    };
 
-    //logit("abendIt", "start");
+    this.deQueue = function () {
+        var data_entry;
+        var data;
 
-    i = 0;
-    p = queue_val.head;
-    while (p) {
-        p = p.next;
-        i += 1;
-    }
-    if (i !== queue_val.size) {
-        abend("abendIt", "head: size=" + queue_val.size + " i=" + i);
-    }
+        this.abendIt();
 
-    i = 0;
-    p = queue_val.tail;
-    while (p) {
-        p = p.prev;
-        i += 1;
-    }
-    if (i !== queue_val.size) {
-        abend("abendIt", "tail: size=" + queue_val.size + " i=" + i);
-    }
+        if (!this.head) {
+            data_entry = null;
+            data = null;
+        } else if (this.head === this.tail) {
+            this.size -= 1;
+            data_entry = this.head;
+            data = data_entry.data;
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.size -= 1;
+            data_entry = this.head;
+            data = data_entry.data;
+            this.head = this.head.next;
+            this.head.prev = null;
+        }
 
-    //logit("abendIt", "end");
-}
+        if (data_entry) {
+            this.logit("deQueue", "data=" + data_entry.data);
+            holder_pool.free(data_entry);
+        }
+        else {
+            this.logit("deQueue", "6000");
+        }
 
-function debug(debug_val, str1_val, str2_val) {
-    if (debug_val) {
-        logit(str1_val, "==" + str2_val);
-    }
-}
+        this.abendIt();
+        return data;
+    };
 
-function abend (str1_val, str2_val) {
-    util.utilAbend("QueueModule." + str1_val, str2_val);
-}
+    this.removeElement = function (func_val, input_val1, input_val2, input_val3) {
+        this.abendIt();
 
-function logit (str1_val, str2_val) {
-    util.utilLogit("QueueModule." + str1_val, str2_val);
+        var p = this.head;
+        while (p) {
+            this.debug(false, "removeElement", "in while loop");
+            if (func_val(p.data, input_val1, input_val2, input_val3)) {
+                this.debug(false, "removeElement", "found");
+                if (p.prev) {
+                    p.prev.next = p.next;
+                } else {
+                    this.head = p.next;
+                }
+                if (p.next) {
+                    p.next.prev = p.prev;
+                } else {
+                    this.tail = p.prev;
+                }
+                this.size -= 1;
+                return;
+            }
+            p = p.next;
+        }
+        this.abendIt();
+        this.debug(false, "removeElement", "not found");
+    };
+
+    this.searchIt = function (func_val, input_val1, input_val2, input_val3) {
+        var p = this.head;
+        while (p) {
+            this.debug(false, "searchIt", "in while loop");
+            if (func_val(p.data, input_val1, input_val2, input_val3)) {
+            this.debug(false, "searchIt", "found");
+                return p.data;
+            }
+            p = p.next;
+        }
+        this.debug(false, "searchIt", "not found");
+        return null;
+    };
+
+    this.abendIt = function () {
+        var i = 0;
+        var p = this.head;
+        while (p) {
+            p = p.next;
+            i += 1;
+        }
+        if (i !== this.size) {
+            this.abend("abendIt", "head: size=" + this.size + " i=" + i);
+        }
+
+        i = 0;
+        p = this.tail;
+        while (p) {
+            p = p.prev;
+            i += 1;
+        }
+        if (i !== this.size) {
+            this.abend("abendIt", "tail: size=" + this.size + " i=" + i);
+        }
+    };
+
+    this.debug = function (debug_val, str1_val, str2_val) {
+        if (debug_val) {
+            this.logit(str1_val, "==" + str2_val);
+        }
+    };
+
+    this.abend = function (str1_val, str2_val) {
+        this.utilModule().abend(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.logit = function (str1_val, str2_val) {
+        this.utilModule().logit(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.theUtilModule = require("./util_module.js");
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
 }
