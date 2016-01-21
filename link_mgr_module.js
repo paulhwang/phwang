afb673eadb1c6f46cd7a6bc5b64e4bb6216feb6f
Paul Hwang <paul.hwang@getac.com.tw>
2016-01-21 13:59:20 +0800

    link mgr

--- link_mgr_module.js
+++ link_mgr_module.js
@@ -6,53 +6,34 @@
 
 module.exports = {
     search: function (my_name_val, link_id_val) {
-        return searchLink(my_name_val, link_id_val);
+        return theLinkMgrObject.searchLink(my_name_val, link_id_val);
     },
 
     search_and_create: function (my_name_val, link_id_val) {
-        return searchAndCreate(my_name_val, link_id_val);
+        return theLinkMgrObject.searchAndCreate(my_name_val, link_id_val);
     },
 
     remove_link: function (link_val) {
-        removeLink(link_val);
+        theLinkMgrObject.removeLink(link_val);
     },
 
     get_name_list: function () {
-        return getNameList();
+        return theLinkMgrObject.getNameList();
     },
 
     malloc: function (my_name_val) {
-         return mallocIt(my_name_val);
+         return theLinkMgrObject.mallocIt(my_name_val);
     },
 
     free: function (entry_val) {
-        freeIt(entry_val);
+        theLinkMgrObject.freeIt(entry_val);
     },
 };
 
-var util = require("./util_module.js");
-var queue = require("./queue_module.js");
-var link_pool = require("./link_pool_module.js");
-
-var link_queue = queue.malloc();
-
-function searchLink(my_name_val, link_id_val) {
-    debug(false, "searchIt", my_name_val + " " + link_id_val);
-    return queue.search(link_queue, compareLink, my_name_val, link_id_val);
-}
-
-function searchAndCreate(my_name_val, link_id_val) {
-    var link = queue.search(link_queue, compareLink, my_name_val, link_id_val);
-    if (!link) {
-        link = link_pool.malloc(my_name_val);
-        debug(false, "searchAndCreate", "malloc link: name=" + link.my_name + "=link_id=" + link.link_id);
-        queue.enqueue(link_queue, link);
-    }
-    return link;
-}
+var theLinkMgrObject = new LinkMgrObject();
 
 function compareLink (link_val, my_name_val, link_id_val) {
-    debug(false, "compareIt", my_name_val + ":" + link_val.my_name);
+    //debug(false, "compareIt", my_name_val + ":" + link_val.my_name);
     if (my_name_val !== link_val.my_name) {
         return false;
     }
@@ -62,55 +43,6 @@ function compareLink (link_val, my_name_val, link_id_val) {
         return (link_id_val === link_val.link_id);
     }
 }
-
-function removeLink (link_val) {
-    logit("removeLink", "my_name=" + link_val.my_name + " link_id=" + link_val.link_id);
-    queue.remove(link_queue, compareLink, link_val.my_name, link_val.link_id);
-}
-
-function getNameList () {
-    var queue_element, link, i;
-    var name_array = [];
-
-    i = 0;
-    queue_element = link_queue.tail;
-    while (queue_element) {
-        link = queue_element.data;
-        name_array[i] = link.my_name;
-        i += 1;
-        queue_element = queue_element.prev;
-    }
-    return name_array;
-}
-
-function mallocIt(my_name_val) {
-    "use strict";
-    var acc = link_pool.malloc(my_name_val);
-    return acc;
-}
-
-function freeIt(entry_val) {
-    "use strict";
-}
-
-function abendIt() {
-}
-
-function debug(debug_val, str1_val, str2_val) {
-    if (debug_val) {
-        logit(str1_val, "==" + str2_val);
-    }
-}
-
-function abend (str1_val, str2_val) {
-    util.utilAbend("LinkMgrModule." + str1_val, str2_val);
-}
-
-function logit (str1_val, str2_val) {
-    util.utilLogit("LinkMgrModule." + str1_val, str2_val);
-}
-
-var theLinkMgrObject = new LinkMgrObject();
 
 function LinkMgrObject() {
     "use strict";
@@ -127,38 +59,38 @@ function LinkMgrObject() {
         return this.theQueueModule;
     };
 
-    this.sessionPoolModule = function () {
-        return this.theSessionPoolModule;
+    this.linkPoolModule = function () {
+        return this.theLinkPoolModule;
     };
 
-    this.sessionQueue = function () {
-        return this.theSessionQueue;
+    this.linkQueue = function () {
+        return this.theLinkQueue;
     };
 
     this.searchLink = function (my_name_val, link_id_val) {
         this.debug(false, "searchIt", my_name_val + " " + link_id_val);
-        return this.queueModule().search(link_queue, compareLink, my_name_val, link_id_val);
+        return this.queueModule().search(this.linkQueue(), compareLink, my_name_val, link_id_val);
     };
 
     this.searchAndCreate = function (my_name_val, link_id_val) {
-        var link = this.queueModule().search(link_queue, compareLink, my_name_val, link_id_val);
+        var link = this.queueModule().search(this.linkQueue(), compareLink, my_name_val, link_id_val);
         if (!link) {
-            link = link_pool.malloc(my_name_val);
+            link = this.linkPoolModule().malloc(my_name_val);
             this.debug(false, "searchAndCreate", "malloc link: name=" + link.my_name + "=link_id=" + link.link_id);
-            this.queueModule().enqueue(link_queue, link);
+            this.queueModule().enqueue(this.linkQueue(), link);
         }
         return link;
     };
 
     this.removeLink = function (link_val) {
         this.logit("removeLink", "my_name=" + link_val.my_name + " link_id=" + link_val.link_id);
-        this.queueModule().remove(link_queue, compareLink, link_val.my_name, link_val.link_id);
+        this.queueModule().remove(this.linkQueue(), compareLink, link_val.my_name, link_val.link_id);
     };
 
     this.getNameList = function () {
         var name_array = [];
         var i = 0;
-        var queue_element = link_queue.tail;
+        var queue_element = this.linkQueue().tail;
         while (queue_element) {
             var link = queue_element.data;
             name_array[i] = link.my_name;
@@ -193,8 +125,8 @@ function LinkMgrObject() {
         this.utilModule().logit(this.objectName() + "." + str1_val, str2_val);
     };
 
-    //this.theUtilModile = require("./util_module.js");
-    //this.theSessionPoolModule = require("./session_pool_module.js");
-    //this.theQueueModule = require("./queue_module.js");
-    //this.theSessionQueue = this.queueModule().malloc();
+    this.theUtilModile = require("./util_module.js");
+    this.theLinkPoolModule = require("./link_pool_module.js");
+    this.theQueueModule = require("./queue_module.js");
+    this.theLinkQueue = this.queueModule().malloc();
 }
